cc99d3e48dd96736e055c7c2adf81edc922dfd8c
Matthew Golden <mgolden91@gmail.com>
2016-05-17 16:51:28 -0500

    added break option

--- app/scripts/controllers/timeclock.js
+++ app/scripts/controllers/timeclock.js
@@ -1,29 +1,54 @@
 (function(){
     function TimeClock($interval){
         var vm = this;
-        vm.title = "Time Left"
-        vm.workTime = 1500;
-        vm.breakTime = 300;
+        vm.onBreak = false;
+        vm.title = "Time Left";
+        vm.buttonTitle = "Work!!!";
+        vm.workTime = 05;
+        vm.breakTime = 10;
+        vm.showButton = true;
         var countDown;
         
+        
         vm.countDown = function() {
+            vm.showButton = false;
             countDown = $interval(function() {
                 if (vm.workTime > 0) {
                     vm.workTime -= 1;
                 } else {
                     $interval.cancel(countDown);
+                    vm.onBreak = !vm.onBreak;
+                    vm.showButton = true;
+                    if (vm.onBreak == true){
+                        vm.buttonTitle = "Break?";
+                    } else {
+                        vm.buttonTitle = "Work?";
+                    }
                 }
             }, 1000);
         }
 
         vm.takeabreak = function(){
             vm.title = "Break Time Left";
+            vm.buttonTitle = "Break!!!";
             vm.workTime = vm.breakTime;
+            vm.onBreak = true;
+            vm.countDown();
         };
        
         vm.resetWorkCount = function(){
             vm.title = "Work Time Left ";
-            vm.workTime = 1500;
+            vm.workTime = 05;
+            vm.onBreak = false;
+            vm.countDown();
+        };
+        
+        vm.finalCountDown = function(){
+          if (vm.onBreak == false){
+              vm.resetWorkCount();      
+          } else if (vm.onBreak == true){
+              vm.takeabreak();
+          }  
         };
         
     }
